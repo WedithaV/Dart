4934b6efd0b8b3a9ccf53131a757eef02ebe4f8f
WedithaV <wedithav@gmail.com>
2025-03-04 12:18:30 +0530

    Funtions

---
+++ functions.dart
@@ -0,0 +1,12 @@
+void main(){
+  print(addition(1,4));
+  print(subtraction(4, 9));
+}
+
+int addition(int num1, int num2){
+  return num1 + num2;
+}
+
+int subtraction(int num1, int num2){
+  return num1 - num2;
+}
